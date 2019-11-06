64c130ca6dfd9cc7d7f0a10cbac234172083c0fd
Vladimir Nikolic <vladimirnikolic24@gmail.com>
2019-11-06 21:40:28 +0100

    update

---
+++ src/Gallery.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+
+
+class Gallery extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Gallery page</h1>
+      </div>
+    );
+  }
+}
+
+export default Gallery;
---
+++ src/Nav.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+
+
+
+class Nav extends Component {
+  render() {
+    return (
+      <h3>logo</h3>
+        <ul>
+          <li>Movies</li>
+          <li>gallery</li>
+        </ul>
+    );
+  }
+}
+
+export default Nav;
